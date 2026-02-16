456fc5433e626d9b0fd9cab7ffdb77b6875250db
oyi1792002 <muhdalbukhary9@gmail.com>
2026-02-16 18:44:55 +0800

    Delete haircules_project directory

--- haircules_project/face_analysis/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FaceAnalysisConfig(AppConfig):
-    name = 'face_analysis'
--- haircules_project/face_analysis/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path 
-from . import views
-
-urlpatterns = [
-    path('analyze/', views.analyze_face, name='analyze_face'),
-]
